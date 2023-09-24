8d4f5d51b367162a2326cf95b0cb1b9e9a881a3c
Scecil044 <scecil072@gmail.com>
2023-09-24 07:54:44 +0300

    pre-production updates before hosting

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -18,6 +19,11 @@ mongoose
     console.log(error);
   });
 const app = express();
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use(express.json());
 app.use(cookieParser());
